654088f65e135eda898d40dfb7c91269252016f6
Anupreeta Mishra <anupreeta.mishra@Eternus.local>
2017-08-04 17:35:51 +0530

    Stock Management  final changes

--- Stocks/stockMangement/stockMangement.js
+++ Stocks/stockMangement/stockMangement.js
@@ -1,209 +1,285 @@
-//AngularJS controller to approve stockption request
-
-function stockApprovalCtl($scope, $http) {
-    /// Add your stuff here
+function stockManagementCtl($scope, $http) {
     var vm = $scope;
-    vm.status = 'OK';
-    vm.userDetails = '';
-    vm.Item_x0020_CodeId = '';
-    vm.listName = 'RnR Product Catalog Approval';
-    vm.listNameProduct = 'RnR Product Catalog';
-    vm.rejectFlag = '';
-    //empSelect = 'Redemption_x0020_Approved_x0020_/Title,Emp_x0020_ID/First_x0020_Name,Emp_x0020_ID/Last_x0020_Name,Item_x0020_Code/Item_x0020_Code,Item_x0020_Code/Item_x0020_Name,Item_x0020_Code/Points,Item_x0020_Code/Quantity,Item_x0020_Code/Items_x0020_Redemend,Item_x0020_Code/Available_x0020_Quantity,*';
-    empSelect = 'Approved_x0020_By/Title,Item_x0020_Code/Item_x0020_Code,*';
-    //empExpand = 'Redemption_x0020_Approved_x0020_/Title,Emp_x0020_ID/First_x0020_Name,Emp_x0020_ID/Last_x0020_Name,Item_x0020_Code/Item_x0020_Code,Item_x0020_Code/Item_x0020_Name,Item_x0020_Code/Points,Item_x0020_Code/Quantity,Item_x0020_Code/Items_x0020_Redemend,Item_x0020_Code/Available_x0020_Quantity';
-    empExpand = 'Approved_x0020_By/Title,Item_x0020_Code/Item_x0020_Code';
+    vm.RnRProductCatalog = 'RnR Product Catalog';
+    vm.ImageList = 'RnR Product Images';
+    vm.ApprovalList = 'RnR Product Catalog Approval';
+    vm.ItemDetails = [];
+    vm.CheckItems = '';
+    vm.id = '';
+    vm.flag = '';
+    vm.IsVisible = false;
+    vm.SaveButton = false;
+    vm.AddButton = true;
+    vm.ClearButton = true;
+    vm.ClearEditButton = false;
+    vm.checkID = '';
+    vm.isDisabledQuantity = false;
+    vm.isTotalDisabledQuantity = true;
+    itemSelect = 'Item_x0020_Code/Item_x0020_Code,*';
+    itemExpand = 'Item_x0020_Code/Item_x0020_Code';
+    Active = 'Active';
     Approved = 'Approved';
     Pending = 'Pending';
     Rejected = 'Rejected';
-    vm.itemID = '';
-    vm.itemCatalogID = '';
-    approvedFilter = 'Status eq \'' + Approved + '\' or Status eq \'' + Rejected + '\'';
-    pendingFilter = 'Status eq \'' + Pending + '\'';
-
-    vm.approvedOptions = {
-        select: empSelect,
-        expand: empExpand,
-        filter: approvedFilter
-    };
+    orderbyModified = 'Modified desc';
+    // pendingFilter = 'Status eq \'' + Pending + '\' or Status eq \'' + Rejected + '\'';
+    pendingFilter = '((Status eq \'' + Pending + '\' ) or ( Status eq \'' + Rejected + '\' )) and (Item_x0020_Status eq \'' + Active + '\')';
+    // approvedFilter = 'Status eq \'' + Approved + '\'';
+    approvedFilter = 'Status eq \'' + Approved + '\' and Item_x0020_Status eq \'' + Active + '\'';
+    // pendingCheckFilter = 'Status eq \'' + Pending + '\' and ID eq \'' + vm.checkID + '\'';
+    // Tabs
+    $(".nav-tabs a").click(function () {
+        $(this).tab('show');
+    });
+    //End
     vm.pendingOptions = {
-        select: empSelect,
-        expand: empExpand,
+        select: itemSelect,
+        expand: itemExpand,
         filter: pendingFilter
     };
-    vm.gridItemsApproved = [];
-    vm.gridItemsPending = [];
-    vm.getUserName = function () {
-        spcrud.getCurrentUser($http).then(function (response) {
-            if (response.status === 200)
-                vm.userDetails = response;
-        }, function (error) {
-            console.log('error', error);
-        });
-    }
-    vm.getUserName();
-    vm.readApproved = function () {
-        spcrud.read($http, vm.listName, vm.approvedOptions).then(function (resp) {
-            if (resp.status === 200)
-                var myJSON = JSON.stringify(resp.data.d.results);
-            vm.gridItemsApproved = resp.data.d.results;
-        }, function (error) {
-            console.log('error', error);
-        });
+    vm.approvedOptions = {
+        filter: approvedFilter
+    };
 
-    };
-    vm.readApproved();
-    vm.readPending = function () {
-        spcrud.read($http, vm.listName, vm.pendingOptions).then(function (resp) {
+    vm.read = function () {
+        spcrud.read($http, vm.RnRProductCatalog, vm.approvedOptions).then(function (resp) {
             if (resp.status === 200)
-                var myJSON = JSON.stringify(resp.data.d.results);
-            vm.gridItemsPending = resp.data.d.results;
+                vm.ItemDetails = resp.data.d.results;
+            var ItemCount = resp.data.d.results.length - 1;
+            if (resp.data.d.results.length == 0) {
+                vm.id = 1;
+            }
+            else {
+                vm.id = resp.data.d.results[ItemCount].ID + 1;
+            }
+            vm.appendimg();
+
         }, function (error) {
-            console.log('error', error);
         });
     };
-    vm.readPending();
-    vm.saveRejectComment = function (comment) {
-        if (vm.rejectFlag == '') {
-            if (vm.itemID != '') {
-                spcrud.update($http, vm.listName, vm.itemID, {
-                    'Reject_x0020_Comment': comment
-                }).then(function (response) {
-                    alert("Comment Added");
-                    vm.itemID = '';
-                    vm.itemCatalogID = '';
-                    comment = '';
-                }, function (error) {
-                    console.log('error', error);
-                });
+    vm.read();
+    // Get RnR Product Catalog Approval(only pending) Items
+    vm.readList = function () {
+        spcrud.read($http, vm.ApprovalList, vm.pendingOptions).then(function (resp) {
+            if (resp.status === 200)
+                vm.ApprovalItemDetails = resp.data.d.results;
+            var ItemCount = resp.data.d.results.length - 1;
+            if (resp.data.d.results.length == 0) {
+                vm.id = 1;
             }
-            if (vm.itemCatalogID != '') {
-                spcrud.update($http, vm.listNameProduct, vm.itemCatalogID, {
-                    'Reject_x0020_Comment': comment
-                }).then(function (response) {
-                    vm.itemID = '';
-                    vm.itemCatalogID = '';
-                    comment = '';
-                }, function (error) {
-                    console.log('error', error);
-                });
+            else {
+                vm.id = resp.data.d.results[ItemCount].ID + 1;
             }
-        }else{
-            alert("You have cancelled rejection!");
+            vm.appendimg();
+        }, function (error) {
+        });
+    };
+    vm.readList();
+    vm.TotalQuantity = function(){
+
+    }
+    // Add New Item to RnR product catalog list
+    vm.AddProduct = function (resp) {
+        var itemId = resp.ID;
+        if (resp.Item_x0020_Name == undefined) {
+            alert("Enter Product Name");
+        }
+        else if (resp.Points == undefined) {
+            alert("Enter Points");
+        }
+        else if (resp.Quantity == undefined) {
+            alert("Enter Quanity");
+        }
+        else {
+
+            spcrud.create($http, vm.RnRProductCatalog, { 'Title': 'Item Adding', 'Status': 'Pending', 'Item_x0020_Code': 'ESPP77100' + vm.id, 'Points': resp.Points, 'Quantity': 0, 'Updated_x0020_Quantity': resp.Quantity, 'Item_x0020_Name': resp.Item_x0020_Name, 'Item_x0020_Description': resp.Item_x0020_Description, 'Item_x0020_Status': 'Active' }
+            ).then(function (resp) {
+                vm.itemId = resp.data.d.ID;
+                spcrud.create($http, vm.ApprovalList, { 'Title': 'Item Adding', 'Status': 'Pending', 'Item_x0020_CodeId': vm.itemId, 'Points': resp.data.d.Points, 'Quantity': 0, 'Updated_x0020_Quantity': resp.data.d.Updated_x0020_Quantity, 'Item_x0020_Name': resp.data.d.Item_x0020_Name, 'Item_x0020_Description': resp.data.d.Item_x0020_Description, 'Item_x0020_Status': 'Active' }
+                ).then(function (resp) {
+                    alert("Product Added Successfully! And sent for Approval");
+
+                    vm.readList();
+                    vm.claer();
+                });
+            });
+        }
+    }
+
+    vm.claer = function () {
+        vm.Item = null;
+        vm.SaveButton = vm.SaveButton ? false : false;
+        vm.IsVisible = vm.IsVisible ? false : false;
+        vm.AddButton = vm.AddButton ? true : true;
+    }
+    vm.claerEdit = function () {
+        vm.Item = null;
+        vm.AddButton = vm.AddButton ? false : true;
+        vm.SaveButton = vm.SaveButton ? false : false;
+        vm.IsVisible = vm.IsVisible ? false : true;
+        vm.isDisabledQuantity = false;
+    }
+    // Get Image from RnR Product Images list
+    vm.setFiles = function (element) {
+        vm.$apply(function (vm) {
+            
+            // Turn the FileList object into an Array
+            var reader = new FileReader();
+            var a = reader.readAsBinaryString(element.files[0]);
+            vm.files = []
+            for (var i = 0; i < element.files.length; i++) {
+                vm.files.push(element.files[i])
+            }
+            var fd = new FormData()
+            for (var i in vm.files) {
+                fd.append("uploadedFile", vm.files[i])
+            }
+            spcrud.uploadFile($http, vm.ImageList, element.files[0].name, fd).then(function (resp) {
+                alert(resp.status);
+                vm.itemId = resp.data.d.ID;
+                vm.read();
+            });
+        });
+    };
+
+       vm.OnEdit = function (resp) {
+        
+        if (vm.CheckItems == 'Pending') {
+            alert('Cannot Update Item ,It is already in pending state!');
+        }
+        else {
+            vm.Item = resp;
+            vm.SaveButton = vm.SaveButton ? true : true;
+            vm.IsVisible = vm.IsVisible ? false : true;
+            vm.AddButton = vm.AddButton ? false : false;
+            vm.isDisabledQuantity = true;
+            vm.ClearButton = false;
+            vm.ClearEditButton = true;
         }
 
-
     }
+    vm.read();
+    // Update Item to RnR Product Catalog Approval list
+    vm.UpdateItem = function (resp) {
+        filterListByItemCode = 'Item_x0020_Code/Item_x0020_Code eq \'' + resp.Item_x0020_Code + '\'';
+        //  + '\' and ID eq \'' + vm.checkID + '\''
 
-    vm.updateApproval = function (item, num) {
-        if (num === 1) {
+        vm.filterByItemCode = {
+            select: '*',
+            filter: filterListByItemCode,
+            orderby: orderbyModified
+        };
+        // vm.checkID = resp.ID;
+        if (resp.Item_x0020_Name == undefined) {
+            alert("Enter Item Name");
+        }
+        else if (resp.Points == undefined) {
+            alert("Enter Points");
+        }
+        else if (resp.NewQuantity == undefined) {
+            alert("Enter Quanity");
+        }
+        else if (resp.ID != null) {
 
-            if (confirm("You are trying to approve! ")) {
-                // var sum = item.Items_x0020_Redemend + item.StockQuantity;
-                // var difference = item.Item_x0020_Code.Quantity - item.Item_x0020_Code.Items_x0020_Redemend;
-                // if (sum > difference) {
-                //     alert('We do not have those many items in stock!');
-                // } else {
-                // selectAll = '*';
-                // itemFilter = 'Item_x0020_Code eq \'' + item.Item_x0020_Code + '\'';
-                // vm.productOptions = {
-                //     select: selectAll,
-                //     filter: itemFilter
-                // }
+            // vm.readItem();
+            spcrud.read($http, vm.ApprovalList, vm.filterByItemCode).then(function (response) {
+                if (response.status === 200) {
+                    vm.CheckItems = response.data.d.results[0].Status;
+                }
+                if (vm.CheckItems == 'Pending') {
+                    alert('Cannot Update Item ,It is already in pending state!');
+                    vm.claerEdit();
 
-                // spcrud.read($http, vm.listNameProduct, vm.productOptions).then(function (resp) {
+                }
+                else {
+                vm.flag = 1;
+                    if (vm.flag != '') {
+                        spcrud.create($http, vm.ApprovalList, { 'Title': 'Item Adding', 'Status': 'Pending', 'Item_x0020_CodeId': resp.ID, 'Points': resp.Points, 'Quantity': resp.Quantity, 'Updated_x0020_Quantity': resp.NewQuantity, 'Item_x0020_Name': resp.Item_x0020_Name, 'Item_x0020_Description': resp.Item_x0020_Description }
 
-                // if (resp.status === 200) {
-                // vm.Item_x0020_CodeId = resp.data.d.results[0].ID;
-                spcrud.update($http, vm.listName, item.ID, {
-                    'Status': 'Approved',
-                    'Approved_x0020_ById': vm.userDetails.data.d.PrincipalType,
-                    'Approved_x0020_Date': new Date(),
-                    'Quantity': item.Quantity + item.Updated_x0020_Quantity
-                }).then(function (response) {
-                    if (response.status === 204) {
-                        vm.readPending();
-                        vm.readApproved();
+                        ).then(function (resp) {
+
+                            alert("Product Updated Successfully! And sent for approval.");
+                            vm.read();
+                            vm.claer();
+                            vm.SaveButton = vm.SaveButton ? false : false;
+                            vm.AddButton = vm.AddButton ? true : true;
+                            vm.isDisabledQuantity = false;
+                        });
                     }
-                }, function (error) {
-                    console.log('error', error);
-                });
+                }
+            }, function (error) {
 
-                spcrud.update($http, vm.listNameProduct, item.Item_x0020_CodeId, {
-                    'Status': 'Approved',
-                    'Approved_x0020_ById': vm.userDetails.data.d.PrincipalType,
-                    'Approved_x0020_Date': new Date(),
-                    'Quantity': item.Quantity + item.Updated_x0020_Quantity
-                }).then(function (error) {
-                    console.log('error', error);
-                });
-                // }
-
-                // }, function (error) {
-                //     console.log('error', error);
-                // });
-
-
-                // }
-
-            }
+            });
 
         } else {
-            if (num === 2) {
-                if (confirm("You are trying to reject! ")) {
-                    // selectAll = '*';
-                    // itemFilter = 'Item_x0020_Code eq \'' + item.Item_x0020_Code + '\'';
-                    // vm.productOptions = {
-                    //     select: selectAll,
-                    //     filter: itemFilter
-                    // }
-
-                    // spcrud.read($http, vm.listNameProduct, vm.productOptions).then(function (resp) {
-                    // vm.Item_x0020_CodeId = resp.data.d.results[0].ID;
-                    // if (resp.status === 200) {
-                    spcrud.update($http, vm.listName, item.ID, {
-                        'Status': 'Rejected',
-                        'Approved_x0020_ById': vm.userDetails.data.d.PrincipalType,
-                        'Approved_x0020_Date': new Date(),
-                        //'Quantity': item.Quantity + item.Updated_x0020_Quantity
-                    }).then(function (response) {
-                        if (response.status === 204) {
-                            vm.readPending();
-                            vm.readApproved();
-                            vm.itemID = item.ID;
-                        }
-                    }, function (error) {
-                        console.log('error', error);
-                    });
-                    spcrud.update($http, vm.listNameProduct, item.Item_x0020_CodeId, {
-                        'Status': 'Rejected',
-                        'Approved_x0020_ById': vm.userDetails.data.d.PrincipalType,
-                        'Approved_x0020_Date': new Date(),
-                        //'Quantity': item.Quantity + item.Updated_x0020_Quantity
-                    }).then(function (response) {
-                        if (response.status === 204) {
-                            // vm.readPending();
-                            // vm.readApproved();
-                            vm.itemCatalogID = item.Item_x0020_CodeId;
-                        }
-                    }, function (error) {
-                        console.log('error', error);
-                    });
-                    // }
-
-                    // }, function (error) {
-                    //     console.log('error', error);
-                    // });
-                } else {
-                    vm.rejectFlag = 'False';
-                }
-
-            }
+            alert("Cannot Update Item");
         }
+    }
 
-    };
+    // delete Functionality
+    vm.OnDelete = function (resp) {
+
+        filterListByItemCode = 'Item_x0020_Code/Item_x0020_Code eq \'' + resp.Item_x0020_Code + '\'';
+        //  + '\' and ID eq \'' + vm.checkID + '\''
+        vm.filterByItemCode = {
+            select: '*',
+            filter: filterListByItemCode,
+            orderby: orderbyModified
+        };
+        if (resp.ID != null) {
+            spcrud.read($http, vm.ApprovalList, vm.filterByItemCode).then(function (response) {
+                if (response.status === 200) {
+                    vm.CheckItems = response.data.d.results[0].Status;
+                }
+                if (vm.CheckItems == 'Pending') {
+                    alert('Cannot Delete Item , It is  already in pending state!');
+
+                }
+                else {
+                vm.flag = 1;
+                    if (vm.flag != '') {
+                        spcrud.update($http, vm.RnRProductCatalog, resp.ID, { 'Item_x0020_Status': 'Inactive' }
+
+                        ).then(function (resp) {
+                            spcrud.update($http, vm.ApprovalList, response.data.d.results[0].ID, { 'Item_x0020_Status': 'Inactive' }
+                            ).then(function (resp) {
+                            });
+                            // if (confirm()="")
+                            //{
+                            confirm("Product Deleted Successfully!.");
+                            //}
+
+                            vm.read();
+                        });
+                    }
+                }
+            }, function (error) {
+
+            });
+        } else {
+            alert("Cannot Delete Item");
+        }
+    }
+
+ 
+
+
+   
+    vm.appendimg = function () {
+        vm.ItemDetails.forEach(function (product) {
+            var id = product.Item_x0020_Code;
+            nameFilter = 'Item_x0020_Code eq \'' + id + '\'';
+            var Options = { filter: nameFilter };
+            spcrud.getImg($http, vm.ImageList, Options).then(function (response) {
+                if (response.status === 200)
+                    vm.Url = response.data.d.results;
+                vm.ItemDetails.find(f => f.Item_x0020_Code == id).Url = response.data.d.results;
+            }, function (error) {
+                
+            });
+
+        }, this);
+        
+    }
 }
-
-
-//load
-angular.module('stockApprovalApp', []).controller('stockApprovalCtl', stockApprovalCtl);
+angular.module('stockManagemenApp', []).controller('stockManagementCtl', stockManagementCtl);
