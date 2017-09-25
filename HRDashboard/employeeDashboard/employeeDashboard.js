859f4d049311386f881f6b890b4bf5b26c0faeb6
Anupreeta Mishra <anupreeta.mishra@Eternus.local>
2017-09-25 18:25:40 +0530

    Timesheet

--- HRDashboard/employeeDashboard/employeeDashboard.js
+++ HRDashboard/employeeDashboard/employeeDashboard.js
@@ -1,10 +1,39 @@
 function employeeDashboardCtl($scope, $http, $timeout) {
     var vm = $scope;
     vm.UserId = '';
+   
+    var vm = $scope;
+    vm.loaded = false;
+    vm.status = 'OK';
+    vm.userDetails = '';
     vm.processForm = function() {
         vm.UserId = localStorage.getItem("UserID");
     }
     vm.processForm();
+    vm.listEmployeeTimesheet = 'Employee Timesheet';
+    vm.loaded = false;
+    empSelect = '*';
+    empExpand = '';
+    TimesheetFilter = 'Employee_x0020_ID eq \'' + vm.UserId + '\'';
+    vm.timesheetOptions = {
+        select: empSelect,
+        expand: empExpand,
+        filter: TimesheetFilter
+    };
+    
+    vm.gridItemsTimesheet = [];
+    vm.readTimesheet = function() {
+        spcrud.read($http, vm.listEmployeeTimesheet, vm.timesheetOptions).then(function(resp) {
+            if (resp.status === 200)
+                var myJSON = JSON.stringify(resp.data.d.results);
+            vm.gridItemsTimesheet = resp.data.d.results;
+            vm.loaded = true;
+        }, function(error) {
+            console.log('error', error);
+        });
+
+    };
+    
 }
 //load
 angular.module('employeeDashboardApp', []).controller('employeeDashboardCtl', employeeDashboardCtl);
