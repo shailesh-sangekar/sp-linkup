d0781c3d9709d93e607484c267fa032f9e03494c
Anupreeta Mishra <anupreeta.mishra@Eternus.local>
2017-09-25 19:19:38 +0530

    Changes in Leave, Certificate and Timesheet

--- HRDashboard/employeeDashboard/employeeDashboard.js
+++ HRDashboard/employeeDashboard/employeeDashboard.js
@@ -11,22 +11,36 @@ function employeeDashboardCtl($scope, $http, $timeout) {
     }
     vm.processForm();
     vm.listEmployeeTimesheet = 'Employee Timesheet';
-    vm.listEmployeeLeaves = 'Employee Leaves Master';
+    vm.listEmployeeLeaves = 'Leave Request Master';
+    vm.listEmployeeCertificates = 'Certification Master';
     vm.loaded = false;
     empSelect = '*';
     empExpand = '';
-    TimesheetFilter = 'Employee_x0020_ID eq \'' + vm.UserId + '\'';
+    TimesheetFilter = 'Title eq \'' + vm.UserId + '\'';
+    EmployeeFilter = 'Employee_x0020_ID eq \'' + vm.UserId + '\'';
+    CertFilter=EmployeeFilter+' and Certification_x0020_Date';
     vm.timesheetOptions = {
         select: empSelect,
         expand: empExpand,
         filter: TimesheetFilter
     };
-    
+    vm.certOptions = {
+        select: empSelect,
+        expand: empExpand,
+        filter: CertFilter
+    };
+    vm.employeeOptions = {
+        select: empSelect,
+        expand: empExpand,
+        filter: EmployeeFilter
+    };
     vm.gridItemsTimesheet = [];
     vm.gridItemsLeaves = [];
+    vm.gridItemsCertificates = [];
     vm.readTimesheet = function() {
         spcrud.read($http, vm.listEmployeeTimesheet, vm.timesheetOptions).then(function(resp) {
             if (resp.status === 200)
+                var myJSON = JSON.stringify(resp.data.d.results);
             vm.gridItemsTimesheet = resp.data.d.results;
             vm.loaded = true;
         }, function(error) {
@@ -35,9 +49,19 @@ function employeeDashboardCtl($scope, $http, $timeout) {
 
     };
     vm.readLeaves = function() {
-        spcrud.read($http, vm.listEmployeeLeaves, vm.timesheetOptions).then(function(resp) {
+        spcrud.read($http, vm.listEmployeeLeaves, vm.employeeOptions).then(function(resp) {
             if (resp.status === 200)
             vm.gridItemsLeaves = resp.data.d.results;
+            vm.loaded = true;
+        }, function(error) {
+            console.log('error', error);
+        });
+
+    };
+    vm.readCertificates = function() {
+        spcrud.read($http, vm.listEmployeeCertificates, vm.certOptions).then(function(resp) {
+            if (resp.status === 200)
+            vm.gridItemsCertificates = resp.data.d.results;
             vm.loaded = true;
         }, function(error) {
             console.log('error', error);
