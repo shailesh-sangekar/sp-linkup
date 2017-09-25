9c5c3b8c7e63ab9453f6768f9857cc2623581c5c
Anupreeta Mishra <anupreeta.mishra@Eternus.local>
2017-09-25 16:36:34 +0530

    Employee List

--- HRDashboard/employeeList/employeeList.js
+++ HRDashboard/employeeList/employeeList.js
@@ -1,5 +1,44 @@
 function employeeListCtl($scope, $http, $timeout) {
+    //default data
+    var vm = $scope;
+    vm.status = 'OK';
+    vm.userDetails = '';
+    vm.listEmployeePersonalDetailsMaster = 'Employee Personal Details Master';
+    vm.loaded = false;
+    empSelect = '*';
+    empExpand = '';
+    Resigned = 'Resigned';
+    ResignedFilter = 'Status/Employee_x0020_Status eq \'' + Resigned + '\'';
+    vm.resignedOptions = {
+        select: empSelect,
+        expand: empExpand,
+        filter: ResignedFilter
+    };
+    
+    vm.gridItemsResigned = [];
+    
+    // vm.getUserName = function () {
+    //     spcrud.getCurrentUser($http).then(function (response) {
+    //         if (response.status === 200)
+    //             vm.userDetails = response;
+    //     }, function (error) {
+    //         console.log('error', error);
+    //     });
+    // }
+    vm.getUserName();
+    vm.readResigned = function () {
+        spcrud.read($http, vm.listName, vm.resignedOptions).then(function (resp) {
+            if (resp.status === 200)
+                var myJSON = JSON.stringify(resp.data.d.results);
+            vm.gridItemsResigned = resp.data.d.results;
+        }, function (error) {
+            console.log('error', error);
+        });
 
+    };
+    vm.readResigned();
+    
 }
+
 //load
 angular.module('employeeListApp', []).controller('employeeListCtl', employeeListCtl);
