6f594140cd491279ec08b5848acf904bff504c33
sayali-eternus <sayali.tidke@eternussolutions>
2017-09-29 11:39:55 +0530

    ManageTIcket folder added with AllTIcket Pages in it

---
+++ ManageTicket/AllTicket.js
@@ -0,0 +1,10 @@
+function AllTicketCtl($scope, $http, $timeout) {
+    var vm = $scope;
+    vm.spinnerloaded = false;
+
+
+
+}
+
+//load
+angular.module('AllTicketApp', []).controller('AllTicketCtl', AllTicketCtl);
