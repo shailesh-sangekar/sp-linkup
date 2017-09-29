861c1a995b90c670cc019ab38649a8257aa31e6f
sayali-eternus <sayali.tidke@eternussolutions>
2017-09-29 19:26:30 +0530

    Manage Ticket data binding completed

--- ManageTicket/AllTicket.js
+++ ManageTicket/AllTicket.js
@@ -1,9 +1,78 @@
 function AllTicketCtl($scope, $http, $timeout) {
     var vm = $scope;
     vm.spinnerloaded = false;
+    vm.loaded = false;
+    vm.listESPLServiceDesk = 'ESPL Service Desk';
+    vm.listESPLServiceDepartments = 'ESPL Service Departments';
+
+    Resigned = 'Resigned';
 
 
+    vm.readPeopleList = function() {
+        spcrud.getCurrentUser($http).then(function(response) {
+            if (response.status === 200)
+                var myJSON = JSON.stringify(response.data.d.results);
+            vm.CurrentLoggedInUser = response.data.d.Title;
+            vm.LoggedInUserID = response.data.d.Id;
+            var DeptListFilter = '';
+            for (i = 0; i < response.data.d.Groups.results.length; i++) {
+                // if (vm.GroupFound == false) {
+                if (response.data.d.Groups.results[i].LoginName == 'ServiceDeskLinkupSupport') {
+                    DeptListFilter = DeptListFilter + 'or (Department/Department eq ' + '\'' + 'Linkup Support' + '\') ';
+                } else if (response.data.d.Groups.results[i].LoginName == 'ServiceDeskRMSSupport') {
+                    DeptListFilter = DeptListFilter + 'or (Department/Department eq ' + '\'' + 'RMS Support' + '\') ';
+                } else if (response.data.d.Groups.results[i].LoginName == 'ServiceDeskIT') {
+                    DeptListFilter = DeptListFilter + 'or (Department/Department eq ' + '\'' + 'IT' + '\') ';
+                } else if (response.data.d.Groups.results[i].LoginName == 'ServiceDeskHR') {
+                    DeptListFilter = DeptListFilter + 'or (Department/Department eq ' + '\'' + 'HR' + '\') ';
+                } else if (response.data.d.Groups.results[i].LoginName == 'ServiceDeskFinance') {
+                    DeptListFilter = DeptListFilter + 'or (Department/Department eq ' + '\'' + 'Finance' + '\') ';
+                } else if (response.data.d.Groups.results[i].LoginName == 'ServiceDeskAdmins') {
+                    DeptListFilter = DeptListFilter + 'or (Department/Department eq ' + '\'' + 'Admin' + '\') ';
+                }
 
+            }
+            vm.readlistESPLServiceDesk(DeptListFilter);
+        }, function(error) {
+            console.log('error', error);
+        });
+    };
+
+    vm.readPeopleList();
+
+    vm.readlistESPLServiceDesk = function(DeptListFilter) {
+        var deptFilter = DeptListFilter.substring(3);
+        empSelect = 'Employee/Title,Department/Department,Editor/Title,Author/Title,*';
+        empExpand = 'Employee/Title,Department/Department,Editor/Title,Author/Title';
+        ModeifiedDate = 'Created desc';
+        count = '1000';
+        // deptFilter = DeptListFilter;
+        vm.resignedOptions = {
+            select: empSelect,
+            expand: empExpand,
+            filter: deptFilter,
+            orderby: ModeifiedDate,
+            top: count
+        };
+        spcrud.read($http, vm.listESPLServiceDesk, vm.resignedOptions).then(function(resp) {
+            if (resp.status === 200)
+                var myJSON = JSON.stringify(resp.data.d.results);
+            vm.listESPLServiceDesk = resp.data.d.results;
+            vm.listESPLServiceDesk.forEach(f => {
+                if (f.Created != null) {
+                    var date2 = new Date();
+                    var date1 = new Date(f.Created);
+                    var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+                    f.ageDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+                }
+
+            });
+            vm.spinnerloaded = true;
+        }, function(error) {
+            console.log('error', error);
+        });
+
+    };
 }
 
 //load
