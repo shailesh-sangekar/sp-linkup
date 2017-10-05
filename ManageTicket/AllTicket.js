40ce00023a9a4da3f1d97833d3dbae7a13a4cb9f
sayali-eternus <sayali.tidke@eternussolutions>
2017-10-05 14:20:27 +0530

    tiscket page added create fun

--- ManageTicket/AllTicket.js
+++ ManageTicket/AllTicket.js
@@ -242,9 +242,15 @@ function AllTicketCtl($scope, $http, $timeout) {
         return $scope.mod;
 
     };
-
-
-
+    spcrud.create($http, vm.listServiceDeskComments, {
+        'Comments': 'test',
+        'Status': 'redeem adding',
+        'Predefined_x0020_Concern': 'Pending',
+        'Department': redeemdate,
+        'Service_x0020_Desk_x0020_Id': itemId
+    }).then(function(resp) {
+        vm.value = vm.product[0].Items_x0020_Redemend + quantity;
+    });
 }
 
 function modal() {
